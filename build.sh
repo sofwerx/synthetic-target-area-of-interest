6354cbdd05f517b12df4372caa2457acc68ed58e
Ian Blenke <ian@blenke.com>
2017-10-17 22:37:44 -0400

    Trying to debug DM

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ if [ ! -f /usr/local/bin/node ]; then
   sudo ln -s /usr/bin/nodejs /usr/local/bin/node
 fi
 if [ -n "$DM" ]; then
+  echo -n "$DM" | md5sum
+  echo -n "$DM" | wc -c
   eval $(dmport --import "$DM")
 fi
 docker-compose build
